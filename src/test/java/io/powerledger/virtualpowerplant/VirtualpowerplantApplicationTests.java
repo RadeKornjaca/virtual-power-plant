c326d3627f24986b1378d2f21babeb6de9e51d1e
radisic.rade <radisic.rade@gmail.com>
2022-06-25 14:23:59 +0200

    Initial commit.

---
+++ src/test/java/io/powerledger/virtualpowerplant/VirtualpowerplantApplicationTests.java
@@ -0,0 +1,13 @@
+package io.powerledger.virtualpowerplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualpowerplantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
